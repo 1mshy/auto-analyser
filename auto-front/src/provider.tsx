efcfedb8b767de43d54ac70efbcc0c43a5b0a9d7
Luca Lapenna <imshi1@protonmail.com>
2025-09-17 14:12:52 -0400

    the big reset

--- auto-front/src/provider.tsx
+++
@@ -1,5 +0,0 @@
-import { HeroUIProvider } from '@heroui/system';
-
-export function Provider({ children }: { children: React.ReactNode }) {
-  return <HeroUIProvider>{children}</HeroUIProvider>;
-}
--- src/handlers/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod auth;
-pub mod market_data;
-pub mod watchlist;
-pub mod alerts;
-pub mod stocks;
-pub mod priority;
